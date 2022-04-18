511491b20f17844cd0f00ca8c4dde60edf46ccae
unknown <gerasimoff.timofeii@yandex.ru>
2022-04-18 17:01:10 +0300

    something

--- 14/14.go
+++ 14/14.go
@@ -1,1 +1,30 @@
-package _4
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var i interface{}
+	i = "t43234"
+	defen(i)
+	i = true
+	defen(i)
+	i = 55
+	defen(i)
+	i = make(chan interface{})
+	defen(i)
+}
+
+func defen(i interface{}) {
+	switch i.(type) {
+	case string:
+		fmt.Printf("type %T\n", i)
+	case bool:
+		fmt.Printf("type %T\n", i)
+	case int:
+		fmt.Printf("type %T\n", i)
+	case chan interface{}:
+		fmt.Printf("type %T\n", i)
+	}
+}
--- 19/19.go
+++ 19/19.go
@@ -1,1 +1,14 @@
-package _9
+package main
+
+import "fmt"
+
+func main() {
+	var in string
+	fmt.Println("insert text")
+	_, _ = fmt.Scan(&in)
+	res := []rune(in)
+	for i, j := 0, len([]rune(res))-1; i < j; i, j = i+1, j-1 {
+		res[i], res[j] = res[j], res[i]
+	}
+	fmt.Printf("reversed: %s\n", string(res))
+}
